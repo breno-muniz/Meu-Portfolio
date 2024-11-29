e3dbaeb8085b57f0012b14a9571c89dd3a454e22
breno-muniz <brenomunizsilva@gmail.com>
2024-11-29 00:34:17 -0300

    Atualização de portfolio

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,16 +1,24 @@
-const menuHamburguer = document.querySelector(".menu-hamburguer") 
-menuHamburguer.addEventListener('click', ()=> {
-    toggleMenu();
-})
+function scrollHeader(){
+    const nav = document.getElementById('header');
+    if(this.scrollY >= 50) nav.classList.add('active-header'); else nav.classList.remove('active-header')
+}
 
-function toggleMenu(){
-    const nav = document.querySelector('.nav-responsive');
-    menuHamburguer.classList.toggle('change');
+window.addEventListener('scroll', scrollHeader)
 
-    if (menuHamburguer.classList.contains('change')){
-        nav.style.display = 'block';
-    
-    }else {
-        nav.style.display = 'none'
+
+//MENU MOBILE
+
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId);
+    const nav = document.getElementById(navId);
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('active-menu-mobile');
+            toggle.classList.toggle('active-bx');
+        })
     }
 }
+
+showMenu('bx', 'menu-mobile')
+
